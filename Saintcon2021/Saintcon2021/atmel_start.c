8dd8e28f3b99850aa4b9e6f72b32ef50bea96780
compukidmike <11616206+compukidmike@users.noreply.github.com>
2021-08-18 21:34:04 -0600

    Add Touch Wheel

--- Saintcon2021/Saintcon2021/atmel_start.c
+++ Saintcon2021/Saintcon2021/atmel_start.c
@@ -8,5 +8,5 @@ void atmel_start_init(void)
 	system_init();
 
 	//This causes enough problems that the chip won't get to main
-	//touch_init();
+	touch_init();
 }
---
+++ Saintcon2021/Saintcon2021/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void touch_status_display(void);
+
+
+
+#endif /* MAIN_H_ */
