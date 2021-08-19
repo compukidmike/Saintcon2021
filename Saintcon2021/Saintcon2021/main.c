7f4822095d9978365ff63ca5fe9ac8f3b47ae86b
compukidmike <11616206+compukidmike@users.noreply.github.com>
2021-08-18 22:22:32 -0600

    Touch Test Code

--- Saintcon2021/Saintcon2021/main.c
+++ Saintcon2021/Saintcon2021/main.c
@@ -5,8 +5,6 @@
 //#include "touch_example.h"
 
 volatile uint8_t measurement_done_touch;
-uint8_t key_status = 0;
-
 uint8_t  scroller_status   = 0;
 uint16_t scroller_position = 0;
 
@@ -30,8 +28,7 @@ int main(void)
 	LCD_Init();
 	
 
-	//LCD_FillRect(0, 0, 240, 240, RGB(10,10,200));
-	LCD_FillRect(0, 0, 240, 240, 0x8800);
+	LCD_FillRect(0, 0, 240, 240, RGB(10,10,200));
 
 	canvas_clearScreen(RGB(10,10,200));
 	canvas_drawText(80,100, "Magic", RGB(255,255,255));
@@ -47,7 +44,7 @@ int main(void)
 		
 		if(gpio_get_pin_level(PIN_PA27)){
 			//gpio_set_pin_level(PIN_PA21,true);
-			//pwm_disable(&PWM_0);
+			pwm_disable(&PWM_0);
 		} else {
 			//gpio_set_pin_level(PIN_PA21,false);
 			pwm_enable(&PWM_0);
@@ -58,96 +55,11 @@ int main(void)
 		//touchWheel = getTouchWheelPostion();
 		static int x1=0,x2=0,y1=0,y2=0;
 		double angle = (((double)(scroller_position)/256)*2*M_PI) + (1.5*M_PI);
-		LCD_DrawLine(x1,y1,x2,y2,0x8800);
+		LCD_DrawLine(x1,y1,x2,y2,RGB(10,10,200));
 		x1 = 80*cos(angle)+120;
 		y1 = 80*sin(angle)+120;
 		x2 = 100*cos(angle)+120;
 		y2 = 100*sin(angle)+120;
 		LCD_DrawLine(x1,y1,x2,y2,0xFFFF);
-	}
-}
-
-
-void touch_status_display(void)
-{
-	key_status = get_sensor_state(0) & KEY_TOUCHED_MASK;
-	if (0u != key_status) {
-		// LED_ON
-		//pwm_enable(&PWM_0);
-		} else {
-		// LED_OFF
-		//pwm_disable(&PWM_0);
-	}
-	key_status = get_sensor_state(1) & KEY_TOUCHED_MASK;
-	if (0u != key_status) {
-		// LED_ON
-		//pwm_enable(&PWM_0);
-		} else {
-		// LED_OFF
-		//pwm_disable(&PWM_0);
-	}
-	key_status = get_sensor_state(2) & KEY_TOUCHED_MASK;
-	if (0u != key_status) {
-		// LED_ON
-		//pwm_enable(&PWM_0);
-		} else {
-		// LED_OFF
-		//pwm_disable(&PWM_0);
-	}
-
-	scroller_status   = get_scroller_state(0);
-	scroller_position = get_scroller_position(0);
-	// Example: 8 bit scroller resolution. Modify as per requirement.
-	scroller_position = scroller_position >> 5;
-	// LED_OFF
-	if (0u != scroller_status) {
-		switch (scroller_position) {
-			case 0:
-			// LED0_ON
-			pwm_enable(&PWM_0);
-			break;
-
-			case 1:
-			// LED1_ON
-			//pwm_enable(&PWM_0);
-			break;
-
-			case 2:
-			// LED2_ON
-			//pwm_enable(&PWM_0);
-			break;
-
-			case 3:
-			// LED3_ON
-			//pwm_enable(&PWM_0);
-			break;
-
-			case 4:
-			// LED4_ON
-			pwm_enable(&PWM_0);
-			break;
-
-			case 5:
-			// LED5_ON
-			//pwm_enable(&PWM_0);
-			break;
-
-			case 6:
-			// LED6_ON
-			//pwm_enable(&PWM_0);
-			break;
-
-			case 7:
-			// LED7_ON
-			//pwm_enable(&PWM_0);
-			break;
-
-			default:
-			// LED_OFF
-			
-			break;
-		}
-	} else {
-		pwm_disable(&PWM_0);
 	}
 }
