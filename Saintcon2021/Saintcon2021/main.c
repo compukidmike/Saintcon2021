d69bf0dc489ce04b63804c29e746b3506057da34
Professor Plum <waylon.grange@gmail.com>
2021-09-11 18:11:11 -0600

    merge + added menu art

--- Saintcon2021/Saintcon2021/main.c
+++ Saintcon2021/Saintcon2021/main.c
@@ -14,6 +14,7 @@ bool back_event = false;
 
 #include "FrameBuffer.h"
 #include "flash.h"
+<<<<<<< Updated upstream
 #include "eeprom.h"
 
 extern uint16_t bird_raw[];
@@ -38,6 +39,10 @@ void Timer_touch_init(void)
 	timer_add_task(&Timer, &Timer_task1);
 	timer_start(&Timer);
 }
+=======
+
+extern uint16_t bird_raw[];
+>>>>>>> Stashed changes
 
 int main(void)
 {
